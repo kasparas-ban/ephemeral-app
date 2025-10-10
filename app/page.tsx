5cd5dcd3de4aea0e14d073c5a7f86ff54962e9c3
kasparasb <kasparasb@wix.com>
2025-10-10 15:13:43 +0300

    Minor rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import EphemeralClass from "@/components/ephemeral/ephemeral";
+import Ephemeral from "@/components/ephemeral/ephemeral";
 
 export default function Home() {
   return (
     <div className="font-sans">
       <main className="flex items-center justify-center h-screen p-4">
-        <EphemeralClass />
+        <Ephemeral />
       </main>
     </div>
   );
