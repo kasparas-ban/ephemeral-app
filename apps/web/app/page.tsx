131e1774982dd0c59ae7371c9b52f0c5f9797c8a
kasparas-ban <kasparas.baniulis@gmail.com>
2025-10-25 16:30:41 +0300

    Introduced global canvas

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,24 @@
+import WorldCanvas from "@/components/canvas/WorldCanvas";
 import Ephemeral from "@/components/ephemeral/ephemeral";
 
 export default function Home() {
   return (
     <div className="font-sans">
       <main className="flex items-center justify-center h-screen p-4">
+        <div className="absolute inset-0">
+          <WorldCanvas>
+            <IncomingEphemerals />
+          </WorldCanvas>
+        </div>
+
         <Ephemeral />
       </main>
     </div>
+  );
+}
+
+function IncomingEphemerals() {
+  return (
+    <div className="w-20 h-20 bg-red-500 translate-x-[200px] translate-y-[100px]" />
   );
 }
